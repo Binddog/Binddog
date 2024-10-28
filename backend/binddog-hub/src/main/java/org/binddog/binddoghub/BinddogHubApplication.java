5f91e884bfe4cd4476b7f6efb915875e63a0afb6
김진이 <jinniek48@gmail.com>
2024-10-28 16:56:46 +0900

    🎉 Init: 프로젝트 생성

---
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/BinddogHubApplication.java
@@ -0,0 +1,13 @@
+package org.binddog.binddoghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinddogHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinddogHubApplication.class, args);
+	}
+
+}
