5ae5f6a58827a7b326ea29755f464181dbd0c441
SeungHuiCho <122choco@naver.com>
2024-10-29 14:46:16 +0900

    ✨ Feat: 사이드 네비게이션 바 추가
    플로우 리스트 페이지에 사이드 네비게이션 바 추가했음

--- frontend/binddog-core/src/Component/SideNav.jsx
+++ frontend/binddog-core/src/Component/SideNav.jsx
@@ -1,14 +1,42 @@
+import { Box } from '@mui/material';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useTheme } from "@mui/material/styles";
 
-function SideNav() {
+function SideNav({li}) {
+  const theme = useTheme();
+
   return (
-    <div>
-      <div>
-        <p>테스트용1</p>
-        <p>테스트용2</p>
-      </div>
-    </div>
+    <Box
+      sx={{
+        width: '300px',
+        height: '100%',
+        bgcolor: theme.palette.primary.dark,
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '30px',
+        alignItems: 'flex-start',
+      }}
+    >
+      <Box>
+        <p>나중에 햄버거 버튼 들어갈 곳(안들어가도 되긴함)</p>
+      </Box>
+      <Box>
+        <h2>유저의 라이브러리</h2>
+      </Box>
+      <Box
+        sx={{
+          color: theme.palette.text.secondary,
+          fontSize: theme.fontSize.large,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+        }}
+      >
+        {li.map((item, index) => (
+          <li>{item}</li>
+        ))}
+      </Box>
+    </Box>
   );
 };
 
--- frontend/binddog-core/src/Page/FlowList.jsx
+++ frontend/binddog-core/src/Page/FlowList.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import SideNav from '../Component/SideNav';
+import { Box, Card } from '@mui/material';
+import { useTheme } from "@mui/material/styles";
 
-function FlowList () {
+function FlowList() {
+  const theme = useTheme();
+
+  const li = ['제목1', '제목2', '제목3']
 
   return (
-    <div>
-      <h1>플로우 확인 페이지(플로우 리스트)</h1>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        height: '100vh',
+        // bgcolor: 'skyblue',
+      }}
+    >
+      <SideNav
+        li={li}
+      />
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          flexGrow: 1,
+          height: '100%',
+        }}
+      >
+        <Box
+          sx={{
+            textAlign: 'center',
+          }}
+        >
+          <h1>플로우 확인 페이지(플로우 리스트)</h1>
+          {/* <p>영역 확인용으로 색 채워둠... 나중에 지울 예정</p> */}
+        </Box>
+        <Card></Card>
+      </Box>
+    </Box>
   );
 };
 
