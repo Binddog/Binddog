dd2c272f9a2dc9bdf1dbe46f701349fd6f8075a0
wnso.kim <wnso.kim@gmail.com>
2024-10-29 17:44:20 +0900

    ✨ Feat: Member 컨트롤러 생성

---
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/member/controller/MemberController.java
@@ -0,0 +1,12 @@
+package org.binddog.binddoghub.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+public class MemberController {
+
+}
