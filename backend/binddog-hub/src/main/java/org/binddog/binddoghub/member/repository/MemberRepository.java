9f8ff525db9e86655d96984059854d2456b60c13
wnso.kim <wnso.kim@gmail.com>
2024-10-29 17:44:03 +0900

    ✨ Feat: Member 레포지토리 생성

---
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/member/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package org.binddog.binddoghub.member.repository;
+
+import org.binddog.binddoghub.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+}
