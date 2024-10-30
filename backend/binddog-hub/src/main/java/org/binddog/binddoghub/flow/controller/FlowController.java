11f54c71a0fadc6213eff77fcb34326dbfae53b6
wnso.kim <wnso.kim@gmail.com>
2024-10-30 11:02:46 +0900

    ✨ Feat: Flow 저장 및 불러오기 더미 컨트롤러 개발

---
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/flow/controller/FlowController.java
@@ -0,0 +1,40 @@
+package org.binddog.binddoghub.flow.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.binddog.binddoghub.global.enums.NoneResponse;
+import org.binddog.binddoghub.global.response.Response;
+import org.binddog.binddoghub.global.response.SuccessResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.binddog.binddoghub.global.enums.SuccessCode.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/projects/")
+public class FlowController {
+
+    @GetMapping("/{projectId}/flows/{flowId}")
+    public ResponseEntity<Response<NoneResponse>> getFlow(
+            @PathVariable Long projectId,
+            @PathVariable Long flowId
+    ){
+        log.info("플로우 불러 오기 성공 {}", "/{projectId}/flows/{flowId}");
+        SuccessResponse<NoneResponse> response = new SuccessResponse<>(GET_FLOW_SUCCESS, NoneResponse.NONE);
+        return Response.success(response);
+    }
+
+    @PostMapping("/{projectId}/flows/{flowId}")
+    public ResponseEntity<Response<NoneResponse>> registerFlow(
+            @PathVariable Long projectId,
+            @PathVariable Long flowId,
+            @RequestParam String flow
+    ){
+        log.info("플로우 저장 성공 {}", "/{projectId}/flows/{flowId}");
+        log.info(flow);
+        SuccessResponse<NoneResponse> response = new SuccessResponse<>(REGISTER_FLOW_SUCCESS, NoneResponse.NONE);
+        return Response.success(response);
+    }
+}
