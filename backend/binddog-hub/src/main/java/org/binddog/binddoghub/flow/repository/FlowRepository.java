870fe2af1df1389b3ca37af2e290ff0bed8da3e2
ys4512558@naver.com <ys4512558@naver.com>
2024-11-01 13:27:18 +0900

    :sparkles: Feat: FlowRepository 프로젝트 ID, 플로우 ID를 통한 삭제 로직 개발

--- backend/binddog-hub/src/main/java/org/binddog/binddoghub/flow/repository/FlowRepository.java
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/flow/repository/FlowRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface FlowRepository extends MongoRepository<Flow, String> {
 
+    void deleteByProjectIdAndId(Long projectId, String id);
+
 }
