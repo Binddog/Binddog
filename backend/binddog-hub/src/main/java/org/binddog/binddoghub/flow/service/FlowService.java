34b772cc17ae2a0b94a25e96fdfae69f13c2c4f8
ys4512558@naver.com <ys4512558@naver.com>
2024-11-01 13:28:46 +0900

    :sparkles: Feat: Flow 생성, 삭제, 조회 인터페이스 메서드 추가 및 구현

--- backend/binddog-hub/src/main/java/org/binddog/binddoghub/flow/service/FlowService.java
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/flow/service/FlowService.java
@@ -1,10 +1,18 @@
 package org.binddog.binddoghub.flow.service;
 
+import org.binddog.binddoghub.flow.dto.req.FlowCreateReq;
 import org.binddog.binddoghub.flow.dto.req.FlowRegisterReq;
+import org.binddog.binddoghub.flow.dto.res.FlowCreateRes;
 import org.binddog.binddoghub.global.enums.NoneResponse;
 import org.binddog.binddoghub.global.response.SuccessResponse;
 
 public interface FlowService {
 
+    SuccessResponse<FlowCreateRes> saveFlow(final Long projectId, FlowCreateReq flowCreateReq);
+
+    SuccessResponse<NoneResponse> loadFlow(final Long projectId, final String flowId, final FlowRegisterReq flowRegisterReq);
+
     SuccessResponse<NoneResponse> updateFlow(final Long projectId, final String flowId, final FlowRegisterReq flowRegisterReq);
+
+    SuccessResponse<NoneResponse> deleteFlow(final Long projectId, final String flowId);
 }
--- backend/binddog-hub/src/main/java/org/binddog/binddoghub/flow/service/FlowServiceImpl.java
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/flow/service/FlowServiceImpl.java
@@ -3,13 +3,16 @@ package org.binddog.binddoghub.flow.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.binddog.binddoghub.flow.document.Flow;
+import org.binddog.binddoghub.flow.dto.req.FlowCreateReq;
 import org.binddog.binddoghub.flow.dto.req.FlowRegisterReq;
+import org.binddog.binddoghub.flow.dto.res.FlowCreateRes;
 import org.binddog.binddoghub.flow.mapper.FlowMapper;
 import org.binddog.binddoghub.flow.repository.FlowRepository;
 import org.binddog.binddoghub.global.enums.NoneResponse;
 import org.binddog.binddoghub.global.response.SuccessResponse;
 import org.springframework.stereotype.Service;
 
+import static org.binddog.binddoghub.global.enums.SuccessCode.DELETE_FLOW_SUCCESS;
 import static org.binddog.binddoghub.global.enums.SuccessCode.REGISTER_FLOW_SUCCESS;
 
 @Slf4j
@@ -21,6 +24,27 @@ public class FlowServiceImpl implements FlowService {
     private final FlowRepository flowRepository;
 
     @Override
+    public SuccessResponse<FlowCreateRes> saveFlow(
+            final Long projectId,
+            final FlowCreateReq flowCreateReq
+    ) {
+        Flow flow = flowMapper.toNewFlow(projectId, flowCreateReq);
+
+        flowRepository.save(flow);
+
+        FlowCreateRes response = FlowCreateRes.builder()
+                                         .flowId(flow.getId())
+                                         .build();
+
+        return new SuccessResponse<>(REGISTER_FLOW_SUCCESS, response);
+    }
+
+    @Override
+    public SuccessResponse<NoneResponse> loadFlow(Long projectId, String flowId, FlowRegisterReq flowRegisterReq) {
+        return null;
+    }
+
+    @Override
     public SuccessResponse<NoneResponse> updateFlow(
             final Long projectId,
             final String flowId,
@@ -29,5 +53,14 @@ public class FlowServiceImpl implements FlowService {
         Flow flow = flowMapper.toFlow(projectId, flowId, flowRegisterReq);
         flowRepository.save(flow);
         return new SuccessResponse<>(REGISTER_FLOW_SUCCESS, NoneResponse.NONE);
+    }
+
+    @Override
+    public SuccessResponse<NoneResponse> deleteFlow(
+            final Long projectId,
+            final String flowId
+    ) {
+        flowRepository.deleteByProjectIdAndId(projectId, flowId);
+        return new SuccessResponse<>(DELETE_FLOW_SUCCESS, NoneResponse.NONE);
     }
 }
