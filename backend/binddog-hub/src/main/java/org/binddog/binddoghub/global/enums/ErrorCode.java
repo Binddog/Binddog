2acec573f596bdc228f8f078c31039213de211a6
wnso.kim <wnso.kim@gmail.com>
2024-11-01 15:04:50 +0900

    💡 Comment: ErrorCode 영문 변경

--- backend/binddog-hub/src/main/java/org/binddog/binddoghub/global/enums/ErrorCode.java
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/global/enums/ErrorCode.java
@@ -11,11 +11,11 @@ public enum ErrorCode implements ResponseCode {
     EMAIL_DUPLICATED(HttpStatus.CONFLICT, "Email is duplicated"),
 
     // Common Error Code
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부적 에러가 발생했습니다."),
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "유효하지 않은 요청입니다."),
-    NOT_FOUND(HttpStatus.NOT_FOUND, "해당 요청을 찾을 수 없습니다."),
-    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "허용되지 않은 HTTP 메소드 입니다"),
-    ACCESS_DENIED(HttpStatus.FORBIDDEN, "권한이 없는 요청입니다.");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "An internal server error occurred."),
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Invalid request."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "Request not found"),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "HTTP method not allowed."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "Unauthorized request.");
 
     private final HttpStatus httpStatus;
     private final String message;
