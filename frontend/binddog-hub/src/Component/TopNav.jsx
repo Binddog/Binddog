90a43320dd9abe5292752da84878cc431d0091e1
dlthgml1501 <dlthgml1501@gmail.com>
2024-11-05 12:39:14 +0900

    :recycle: Refactor: TopNav 수정

--- frontend/binddog-hub/src/Component/TopNav.jsx
+++ frontend/binddog-hub/src/Component/TopNav.jsx
@@ -1,38 +1,25 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useTheme } from "@mui/material/styles";
 import { Box, Typography } from "@mui/material";
 
 function TopNav() {
   const theme = useTheme();
-
   const navigate = useNavigate();
 
-  const goToHome = () => {
-    navigate("/");
-  };
-
-  const goToDocs = () => {
-    navigate("/docs");
-  };
-
-  const goToHubList = () => {
-    navigate("/hubList");
-  };
-
-  const SignUp = () => {
-    navigate("/SignUp");
-  };
-
-  const Login = () => {
-    navigate("/Login");
-  };
+  const buttons = [
+    { label: "Home", onClick: () => navigate("/") },
+    { label: "Docs", onClick: () => navigate("/docs") },
+    { label: "SignUp", onClick: () => navigate("/signup") },
+    { label: "Login", onClick: () => navigate("/login") },
+    { label: "FlowList", onClick: () => navigate("/flowList") },
+  ];
 
   return (
     <Box
       sx={{
         width: "100%",
-        height: "80px",
+        height: "50px",
         bgcolor: theme.palette.primary.main,
         padding: "15px 10px",
         display: "flex",
@@ -48,101 +35,28 @@ function TopNav() {
           gap: 3,
         }}
       >
-        <Typography
-          component="button"
-          onClick={goToHome}
-          sx={[
-            theme.typography.h3,
-            {
-              padding: "10px",
-              borderRadius: "10px",
-              border: "none",
-              bgcolor: theme.palette.primary.main,
-              cursor: "pointer",
-              "&:hover": {
-                bgcolor: theme.palette.primary.dark,
+        {buttons.map((button, index) => (
+          <Typography
+            key={index}
+            component="button"
+            onClick={button.onClick}
+            sx={[
+              theme.typography.h3,
+              {
+                padding: "10px",
+                borderRadius: "10px",
+                border: "none",
+                bgcolor: theme.palette.primary.main,
+                cursor: "pointer",
+                "&:hover": {
+                  bgcolor: theme.palette.primary.dark,
+                },
               },
-            },
-          ]}
-        >
-          Home
-        </Typography>
-        <Typography
-          component="button"
-          onClick={goToDocs}
-          sx={[
-            theme.typography.h3,
-            {
-              padding: "10px",
-              borderRadius: "10px",
-              border: "none",
-              bgcolor: theme.palette.primary.main,
-              cursor: "pointer",
-              "&:hover": {
-                bgcolor: theme.palette.primary.dark,
-              },
-            },
-          ]}
-        >
-          Docs
-        </Typography>
-        <Typography
-          component="button"
-          onClick={goToHubList}
-          sx={[
-            theme.typography.h3,
-            {
-              padding: "10px",
-              borderRadius: "10px",
-              border: "none",
-              bgcolor: theme.palette.primary.main,
-              cursor: "pointer",
-              "&:hover": {
-                bgcolor: theme.palette.primary.dark,
-              },
-            },
-          ]}
-        >
-          HubList
-        </Typography>
-        <Typography
-          component="button"
-          onClick={SignUp}
-          sx={[
-            theme.typography.h3,
-            {
-              padding: "10px",
-              borderRadius: "10px",
-              border: "none",
-              bgcolor: theme.palette.primary.main,
-              cursor: "pointer",
-              "&:hover": {
-                bgcolor: theme.palette.primary.dark,
-              },
-            },
-          ]}
-        >
-          SignUp
-        </Typography>
-        <Typography
-          component="button"
-          onClick={Login}
-          sx={[
-            theme.typography.h3,
-            {
-              padding: "10px",
-              borderRadius: "10px",
-              border: "none",
-              bgcolor: theme.palette.primary.main,
-              cursor: "pointer",
-              "&:hover": {
-                bgcolor: theme.palette.primary.dark,
-              },
-            },
-          ]}
-        >
-          Login
-        </Typography>
+            ]}
+          >
+            {button.label}
+          </Typography>
+        ))}
       </Box>
     </Box>
   );
