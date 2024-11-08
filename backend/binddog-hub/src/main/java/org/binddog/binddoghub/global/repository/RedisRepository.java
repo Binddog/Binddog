f293b8d079b3e6f109c3243c251e414434a92b83
김진이 <jinniek48@gmail.com>
2024-11-08 16:35:21 +0900

    :bug: Fix: 토큰이 삭제되지 않는 버그 해결

--- backend/binddog-hub/src/main/java/org/binddog/binddoghub/global/repository/RedisRepository.java
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/global/repository/RedisRepository.java
@@ -1,10 +1,17 @@
 package org.binddog.binddoghub.global.repository;
 
+import java.util.Optional;
 import org.binddog.binddoghub.global.dto.Tokens;
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.transaction.annotation.Transactional;
 
 public interface RedisRepository extends CrudRepository<Tokens, Long> {
-	void deleteByAccessToken(String accessToken);
-	Tokens findByAccessToken(String accessToken);
+	@Transactional
+	default void deleteByAccessToken(String accessToken) {
+		findByAccessToken(accessToken)
+				.ifPresent(this::delete);
+	}
+
+	Optional<Tokens> findByAccessToken(String accessToken);
+
 }
