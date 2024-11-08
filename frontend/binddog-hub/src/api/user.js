2bd5c8df545a1fb35fd8ec00cddc34cc4cd63f45
dlthgml1501 <dlthgml1501@gmail.com>
2024-11-08 17:29:14 +0900

    :recycle: Refactor: API 수정

--- frontend/binddog-hub/src/api/user.js
+++ frontend/binddog-hub/src/api/user.js
@@ -3,24 +3,29 @@ import Axios from "./index";
 
 // 회원가입
 export const signUp = async (email, password) => {
-  const response = await Axios.post("members/sign-up", {email:email, password:password});
-  return response.resultCode;
-}
+  const response = await Axios.post("members/sign-up", { email, password });
+  return response;
+};
 
 // 로그인
 export const logIn = async (email, password) => {
-  const response = await Axios.post("auths/login");
+  const response = await Axios.post("auths/login", { email, password });
   return response.data;
-}
+};
 
 // 로그아웃
 export const logOut = async (auth) => {
-  const response = await Axios.post("auths/logout", { params: { Authorization: auth } });
+  const response = await Axios.post("auths/logout", {
+    params: { Authorization: auth },
+  });
   return response.resultCode;
-}
+};
 
 // 리프레시
 export const refresh = async (accessToken, refreshToken) => {
-  const response = await Axios.post("auths/refresh", { accessToken, refreshToken });
+  const response = await Axios.post("auths/refresh", {
+    accessToken,
+    refreshToken,
+  });
   return response.data;
-}
+};
