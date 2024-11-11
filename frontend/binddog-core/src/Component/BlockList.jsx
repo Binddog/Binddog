3f5c96613bfe69e2165aec50d239e522401d18ae
ys4512558@naver.com <ys4512558@naver.com>
2024-11-11 18:57:47 +0900

    ✨ Feat: Docs를 파싱하여 Block 리스트를 세팅하는 컴포넌트 로직 추가

--- frontend/binddog-core/src/Component/BlockList.jsx
+++ frontend/binddog-core/src/Component/BlockList.jsx
@@ -1,17 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import Block from "./Block";
-import blocksData from "../block.json";
+import { getDocs } from "../api/libraryFlow"
 
-function BlockList({ li = blocksData.blocks, name, addNode }) {
+let idx = 0;
+
+function parseResponse(res) {
+  //TODO: res 200일 때의 response 세팅
+}
+
+function parseRequest(params) {
+  const parameters = [];
+  const pathVariables = [];
+  const headers = [];
+
+  params.forEach((param) => {
+    if (param.in === "path") {
+      parameters.push(param.name)
+    } else if (param.in === "query") {
+      pathVariables.push(param.name);
+    } else if (param.in == "header") {
+      headers.push(param.name)
+    }
+  })
+  return { parameters, pathVariables, headers }
+}
+
+function createBlock(path, method, detail) {
+  const parsedRequest = parseRequest(detail.parameters || []);
+  const parameters = parsedRequest.parameters;
+  const pathVariables = parsedRequest.pathVariables;
+  const headers = parsedRequest.headers;
+
+  return {
+    key: idx,
+    id: idx++,
+    method: method.toUpperCase(),
+    apiName: detail.summary,
+    endpoint: path,
+    // position: item.position,
+    header: headers,
+    parameter: parameters,
+    pathVariable: pathVariables,
+    request: detail.requestBody,
+    response: detail.responses
+  }
+}
+
+function createBlockList(context, docs) {
+  const blockList = new Array();
+  Object.entries(docs).forEach(([path, value]) => {
+    Object.entries(value).forEach(([method, detail]) => {
+      blockList.push(createBlock(`${context}${path}`, method, detail))
+    });
+  });
+  return blockList;
+}
+
+function BlockList({ addNode }) {
+  const [name, setName] = useState("")
   const theme = useTheme();
+  const [li, setLi] = useState([]); // li를 빈 배열로 초기화
+  useEffect(() => {
+    const fetchDocs = async () => {
+      try {
+        const docsData = await getDocs();
+        setName(docsData.info.title);
+
+        const context = docsData.servers[0].url;
+        const paths = docsData.paths;
+
+        console.log(docsData)
+        const temp = createBlockList(context, paths);
+        setLi(temp || []);
+        console.log(temp)
+        console.log("li = ", li)
+        // setLi(createBlockList(context, paths) || []);
+        console.log("temp")
+      } catch (error) {
+        console.error("문서 데이터를 가져오는 중 오류 발생:", error);
+      }
+    };
+
+    fetchDocs();
+  }, []);
+
 
   return (
     <Box
       sx={{
         width: "250px",
-        height: "100%",
+        // height: "100%",
         bgcolor: "#F7F7F7",
         display: "flex",
         flexDirection: "column",
@@ -29,13 +109,13 @@ function BlockList({ li = blocksData.blocks, name, addNode }) {
         {name}
       </Typography>
       {li.map((item) => (
-        <Block
-          key={item.blockId}
-          id={item.blockId}
+        < Block
+          key={item.key}
+          id={item.id}
           method={item.method}
-          apiName={item.name}
+          apiName={item.apiName}
           endpoint={item.endpoint}
-          position={item.position}
+          // position = { item.position }
           header={item.header}
           parameter={item.parameter}
           pathVariable={item.pathVariable}
