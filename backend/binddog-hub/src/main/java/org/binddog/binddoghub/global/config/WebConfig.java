83a3b17fa712dfe0d5b4633dc07d6d0da5d1857c
김준영 <wnso.kim@gmail.com>
2024-11-11 15:47:10 +0900

    🚑️ !HOTFIX: 필터 오류 수정

--- backend/binddog-hub/src/main/java/org/binddog/binddoghub/global/config/WebConfig.java
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/global/config/WebConfig.java
@@ -7,18 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final String[] patterns = {
-            "http://localhost:*",
-            "http://127.0.0.1:*",
-            "https://localhost:*",
-            "https://127.0.0.1:*",
-    };
-
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(patterns)
+                .allowedOriginPatterns(
+                    "http://localhost:[*]",
+                    "https://localhost:[*]"
+                )
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
