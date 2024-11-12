5ef392a5e71795594381f20cf36bc79bc231315f
dlthgml1501 <dlthgml1501@gmail.com>
2024-11-12 15:08:14 +0900

    :recycle: Refactor: api return값 수정

--- frontend/binddog-core/src/api/libraryFlow.js
+++ frontend/binddog-core/src/api/libraryFlow.js
@@ -1,20 +1,23 @@
 // 라이브러리 - 플로우
 
 import Axios from "./index";
+import docsAxios from "./docsAxios";
+
+// 해당 프로젝트의 API 목록 조회
+export const getDocs = async () => {
+  const response = await docsAxios(`/api/v3/api-docs`);
+  return response.data;
+};
 
 // 특정 프로젝트의 단일 플로우 조회
 export const getFlow = async (projectId, flowId) => {
-  const response = await Axios(`projects/${projectId}/flows/${flowId}`, {
-    params: { projectId, flowId },
-  });
+  const response = await Axios(`projects/${projectId}/flows/${flowId}`);
   return response.data;
 };
 
 // 특정 프로젝트의 모든 플로우 조회
 export const getAllFlow = async (projectId) => {
-  const response = await Axios(`projects/${projectId}/flows`, {
-    params: { projectId },
-  });
+  const response = await Axios(`projects/${projectId}/flows`);
   console.log("data: ", response.data.data.flows);
   return response.data.data.flows;
 };
@@ -30,18 +33,16 @@ export const createFlow = async (projectId, title, description) => {
 
 // 특정 프로젝트의 단일 플로우 수정
 export const modifyFlow = async (projectId, flowId, blocks, links, mapping) => {
-  const response = await Axios.put(
-    `projects/${projectId}/flows/${flowId}`,
-    { blocks, links, mapping },
-    { params: { projectId, flowId } }
-  );
+  const response = await Axios.put(`projects/${projectId}/flows/${flowId}`, {
+    blocks,
+    links,
+    mapping,
+  });
   return response.data;
 };
 
 // 특정 프로젝트의 단일 플로우 삭제
 export const deleteFlow = async (projectId, flowId) => {
-  const response = await Axios.delete(`projects/${projectId}/flows/${flowId}`, {
-    params: { projectId, flowId },
-  });
+  const response = await Axios.delete(`projects/${projectId}/flows/${flowId}`);
   return response.data;
 };
--- frontend/binddog-core/src/api/project.js
+++ frontend/binddog-core/src/api/project.js
@@ -15,7 +15,6 @@ export const createProject = async (title, description) => {
 export const getProjects = async () => {
   try {
     const response = await Axios(`projects`);
-    console.log("data: ", response.data);
     return response.data.data;
   } catch (error) {
     console.error("프로젝트 생성 실패:", error);
