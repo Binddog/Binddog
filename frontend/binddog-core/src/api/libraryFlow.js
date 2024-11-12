e8283cd055430bdb1f5b4032ad656854a1c2e0fb
dlthgml1501 <dlthgml1501@gmail.com>
2024-11-12 13:35:04 +0900

    :recycle: Refactor: 플로우 api 수정

--- frontend/binddog-core/src/api/libraryFlow.js
+++ frontend/binddog-core/src/api/libraryFlow.js
@@ -4,30 +4,44 @@ import Axios from "./index";
 
 // 특정 프로젝트의 단일 플로우 조회
 export const getFlow = async (projectId, flowId) => {
-  const response = await Axios(`projects/${projectId}/flows/${flowId}`, { params: { projectId, flowId } });
+  const response = await Axios(`projects/${projectId}/flows/${flowId}`, {
+    params: { projectId, flowId },
+  });
   return response.data;
-}
+};
 
 // 특정 프로젝트의 모든 플로우 조회
 export const getAllFlow = async (projectId) => {
-  const response = await Axios(`projects/${projectId}/flows`, { params: { projectId } });
-  return response.data;
-}
+  const response = await Axios(`projects/${projectId}/flows`, {
+    params: { projectId },
+  });
+  console.log("data: ", response.data.data.flows);
+  return response.data.data.flows;
+};
 
-// 특정 프로젝트의 플로우 생성
+// api/libraryFlow.js
 export const createFlow = async (projectId, title, description) => {
-  const response = await Axios.post(`projects/${projectId}/flows`, { title, description }, { params: { projectId } });
+  const response = await Axios.post(`projects/${projectId}/flows`, {
+    title,
+    description,
+  });
   return response.data;
-}
+};
 
 // 특정 프로젝트의 단일 플로우 수정
 export const modifyFlow = async (projectId, flowId, blocks, links, mapping) => {
-  const response = await Axios.put(`projects/${projectId}/flows/${flowId}`, { blocks, links, mapping }, { params: { projectId, flowId } });
+  const response = await Axios.put(
+    `projects/${projectId}/flows/${flowId}`,
+    { blocks, links, mapping },
+    { params: { projectId, flowId } }
+  );
   return response.data;
-}
+};
 
 // 특정 프로젝트의 단일 플로우 삭제
 export const deleteFlow = async (projectId, flowId) => {
-  const response = await Axios.delete(`projects/${projectId}/flows/${flowId}`, { params: { projectId, flowId } });
+  const response = await Axios.delete(`projects/${projectId}/flows/${flowId}`, {
+    params: { projectId, flowId },
+  });
   return response.data;
-}
+};
