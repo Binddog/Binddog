6eb0e4ac3943bdd0dd6bad8037c4b2d58815a462
dlthgml1501 <dlthgml1501@gmail.com>
2024-11-13 17:49:05 +0900

    :recycle: Refactor: 상단바 수정

--- frontend/binddog-core/src/Component/TopNav.jsx
+++ frontend/binddog-core/src/Component/TopNav.jsx
@@ -11,14 +11,10 @@ function TopNav({ isLogin, handleLogout, email }) {
 
   const buttons = [
     { label: "Home", onClick: () => navigate("/") },
-    { label: "Docs", onClick: () => navigate("/docs") },
     { label: "Projects", onClick: () => navigate("/projects") },
   ];
 
-  const authButtons = [
-    { label: "Login", onClick: () => navigate("/login") },
-    { label: "SignUp", onClick: () => navigate("/signUp") },
-  ];
+  const authButtons = [{ label: "Login", onClick: () => navigate("/login") }];
 
   const userName = email && email.includes("@") ? email.split("@")[0] : "User";
 
@@ -37,11 +33,12 @@ function TopNav({ isLogin, handleLogout, email }) {
     <Box
       sx={{
         height: "50px",
-        bgcolor: theme.palette.primary.main,
+        bgcolor: theme.palette.common.white,
         padding: "10px 20px",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
+        borderBottom: "1px solid lightgrey",
       }}
     >
       <Box
@@ -57,15 +54,15 @@ function TopNav({ isLogin, handleLogout, email }) {
             component="button"
             onClick={button.onClick}
             sx={[
-              theme.typography.h3,
+              theme.typography.sub,
               {
                 padding: "10px",
                 borderRadius: "10px",
                 border: "none",
-                bgcolor: theme.palette.primary.main,
+                bgcolor: theme.palette.common.white,
                 cursor: "pointer",
                 "&:hover": {
-                  bgcolor: theme.palette.primary.dark,
+                  bgcolor: theme.palette.primary.main,
                 },
               },
             ]}
@@ -96,7 +93,7 @@ function TopNav({ isLogin, handleLogout, email }) {
                 variant="beam"
                 colors={["#FB6A4A", "#A1D99B", "#41B6C4", "#FDAE6B", "#C994C7"]}
               />
-              <Typography sx={{ ...theme.typography.h3, marginLeft: "8px" }}>
+              <Typography sx={{ ...theme.typography.sub, marginLeft: "8px" }}>
                 {userName}
               </Typography>
             </Button>
@@ -118,8 +115,8 @@ function TopNav({ isLogin, handleLogout, email }) {
                 }}
                 sx={{
                   ...theme.typography,
-                  fontSize: theme.fontSize.medium,
-                  padding: "10px 20px",
+                  fontSize: theme.fontSize.small,
+                  padding: "10px",
                   color: theme.palette.text.primary,
                 }}
               >
@@ -134,15 +131,15 @@ function TopNav({ isLogin, handleLogout, email }) {
               component="button"
               onClick={button.onClick}
               sx={[
-                theme.typography.h3,
+                theme.typography.sub,
                 {
-                  padding: "10px",
+                  padding: "13px",
                   borderRadius: "10px",
                   border: "none",
-                  bgcolor: theme.palette.primary.main,
+                  bgcolor: theme.palette.common.white,
                   cursor: "pointer",
                   "&:hover": {
-                    bgcolor: theme.palette.primary.dark,
+                    bgcolor: theme.palette.primary.main,
                   },
                 },
               ]}
