85a2b6228b24c87f4213233a1fc4f574dee5118e
dlthgml1501 <dlthgml1501@gmail.com>
2024-11-15 17:20:16 +0900

    :recycle: Refactor: api 수정

--- frontend/binddog-hub/src/api/hubProject.js
+++ frontend/binddog-hub/src/api/hubProject.js
@@ -5,12 +5,16 @@ import Axios from "./index";
 // 허브 프로젝트 조회
 export const getProject = async () => {
   const response = await Axios("projects");
-  return response.data;
+  console.log(response.data.data);
+  return response.data.data;
 };
 
 // 허브 프로젝트 생성
 // 확장 가능성을 생각해서 넣은 것(허브에 다양한 프로젝트가 있는 것)
 export const postProject = async (title, description) => {
-  const response = await Axios.post("projects", { title: title, description: description });
+  const response = await Axios.post("projects", {
+    title: title,
+    description: description,
+  });
   return response.resultCode;
 };
