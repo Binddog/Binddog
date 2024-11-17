da82e333d54609bf6fec0d4121da2d3e5fa1a4ea
서정희 <sjhlko@naver.com>
2024-11-18 02:15:48 +0900

    🐛 Fix: 정적 리소스 경로로 리턴하도록 수정

--- backend/binddog-hub/src/main/java/org/binddog/binddoghub/image/service/impl/MinioServiceImpl.java
+++ backend/binddog-hub/src/main/java/org/binddog/binddoghub/image/service/impl/MinioServiceImpl.java
@@ -1,25 +1,27 @@
 package org.binddog.binddoghub.image.service.impl;
 
-import io.minio.*;
-import io.minio.http.Method;
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
 import lombok.RequiredArgsConstructor;
 import org.binddog.binddoghub.global.enums.ErrorCode;
 import org.binddog.binddoghub.global.enums.NoneResponse;
 import org.binddog.binddoghub.global.handler.AppException;
 import org.binddog.binddoghub.global.response.SuccessResponse;
-import org.binddog.binddoghub.image.UploadImageRequest;
+import org.binddog.binddoghub.image.dto.req.UploadImageReq;
+import org.binddog.binddoghub.image.dto.res.ImageUrlRes;
 import org.binddog.binddoghub.image.service.MinioService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.InputStream;
-import java.net.URI;
-import java.util.Map;
 
 import static org.binddog.binddoghub.global.enums.ErrorCode.BUCKET_INVALID;
 import static org.binddog.binddoghub.global.enums.ErrorCode.IMAGE_GET_ERROR;
 import static org.binddog.binddoghub.global.enums.NoneResponse.NONE;
+import static org.binddog.binddoghub.global.enums.SuccessCode.IMAGE_GET_SUCCESS;
 import static org.binddog.binddoghub.global.enums.SuccessCode.IMAGE_UPLOAD_SUCCESS;
 
 @Service
@@ -35,10 +37,10 @@ public class MinioServiceImpl implements MinioService {
     private String presignedURL;
 
     @Override
-    public SuccessResponse<NoneResponse> uploadFile(MultipartFile file, UploadImageRequest request) {
+    public SuccessResponse<NoneResponse> uploadFile(MultipartFile file, UploadImageReq request) {
         try (InputStream inputStream = file.getInputStream()) {
             ensureBucketExists();
-            String fileName = request.projectId() + "-" + request.flowId() + ".jpg" ;
+            String fileName = request.projectId() + "-" + request.flowId() + ".jpg";
             minioClient.putObject(
                     PutObjectArgs.builder()
                                  .bucket(bucketName)
@@ -54,33 +56,15 @@ public class MinioServiceImpl implements MinioService {
     }
 
     @Override
-    public String getPresignedUrl(Long projectId, Long flowId){
+    public SuccessResponse<ImageUrlRes> getPresignedUrl(Long projectId, Long flowId) {
         try {
             ensureBucketExists();
-            String fileName = projectId + "-" + flowId + ".jpg" ;
-            String presignedUrl = minioClient.getPresignedObjectUrl(
-                    GetPresignedObjectUrlArgs.builder()
-                                             .bucket(bucketName)
-                                             .object(fileName)
-                                             .method(Method.GET)
-                                             .expiry(60 * 60 * 24 * 7) // 7일
-                                             .extraQueryParams(Map.of("response-content-type", "image/jpeg"))
-                                             .build()
-            );
-
-            // Presigned URL의 내부 도메인 부분을 외부 도메인으로 변경
-            URI uri = new URI(presignedUrl);
-            URI externalUri = new URI(
-                    uri.getScheme(), // "http"
-                    uri.getUserInfo(),
-                    presignedURL.replace("http://", ""), // 외부 접근 가능한 호스트
-                    uri.getPort(),
-                    uri.getPath(),
-                    uri.getQuery(),
-                    uri.getFragment()
-            );
-
-            return externalUri.toString();
+            String fileName = projectId + "-" + flowId + ".jpg";
+            String url = presignedURL + "/" + bucketName + "/" + fileName;
+            ImageUrlRes response = ImageUrlRes.builder()
+                                              .url(url)
+                                              .build();
+            return new SuccessResponse<>(IMAGE_GET_SUCCESS, response);
         } catch (Exception e) {
             throw new AppException(IMAGE_GET_ERROR);
         }
