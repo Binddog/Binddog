cc7c7b3d0d3f88c8f7098ba4ce36b5f05a3829d9
ys4512558@naver.com <ys4512558@naver.com>
2024-11-18 12:53:19 +0900

    ✨ Feat: 플로우에 헤더 추가 후 실행하도록 로직 추가

--- frontend/binddog-core/src/Component/Buttons/RunButton.jsx
+++ frontend/binddog-core/src/Component/Buttons/RunButton.jsx
@@ -3,22 +3,22 @@ import { useTheme } from "@mui/material/styles";
 import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
 import { runFlow } from "../../api/runFlow";
 
-const RunButton = ({nodes, edges, addLog, restartLog}) => {
+const RunButton = ({ nodes, edges, addLog, restartLog }) => {
   const theme = useTheme();
 
-  const handleClick = async() => {
+  const handleClick = async () => {
     // reset logs
     restartLog();
-    
-    if(edges && edges.length > 0){
+
+    if (edges && edges.length > 0) {
       console.log(edges)
       let currentEdge = edges.find(edge => edge.source === '0');
       if (!currentEdge) {
         return;
       }
       let currentNode = nodes.find(node => node.id === currentEdge.target);
-      
-      while(currentNode){
+
+      while (currentNode) {
         console.log(`flow call endpoint: ${currentNode.data.endpoint}`);
 
         const endpoint = currentNode.data.endpoint;
@@ -26,8 +26,8 @@ const RunButton = ({nodes, edges, addLog, restartLog}) => {
 
         // newEndpoint 초기화: endpoint에서 {key}를 pathValue의 값으로 대체합니다.
         const newEndpoint = endpoint.replace(/{(\w+)}/g, (match, key) => {
-            // pathValue에 key가 존재하는 경우 해당 값을, 없으면 그대로 유지
-            return pathValue.get(key) || match;
+          // pathValue에 key가 존재하는 경우 해당 값을, 없으면 그대로 유지
+          return pathValue.get(key) || match;
         });
 
         console.log(newEndpoint);
@@ -36,21 +36,25 @@ const RunButton = ({nodes, edges, addLog, restartLog}) => {
         // Map을 일반 객체로 변환
         let paramObject = currentNode.data.paramValue;
         if (paramObject instanceof Map) {
-            paramObject = Object.fromEntries(paramObject); // Map -> Object 변환
+          paramObject = Object.fromEntries(paramObject); // Map -> Object 변환
         }
+        let headerObject = currentNode.data.headerValue;
+        if (headerObject instanceof Map) {
+          headerObject = Object.fromEntries(headerObject); // Map -> Object 변환
+        }
+
         console.log(paramObject);
-      
+        console.log(headerObject);
+
         const result = await runFlow(
           newEndpoint,
           currentNode.data.method,
           paramObject,
-          currentNode.data.paramValue,
-          // currentEdge.data.pathVariable,
-          // currentEdge.data.parameter
+          headerObject
         );
-        
+
         addLog(result.response);
-        if(!result.success){
+        if (!result.success) {
           break;
         }
 
